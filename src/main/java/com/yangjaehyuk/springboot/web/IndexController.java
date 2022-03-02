acfbf9bd921bf3c05c60bbecbae6de80a997b2bb
yangjaehyuk <didwogur28@naver.com>
2022-03-02 16:34:01 +0900

    구글 로그인 연동

--- src/main/java/com/yangjaehyuk/springboot/web/IndexController.java
+++ src/main/java/com/yangjaehyuk/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.yangjaehyuk.springboot.web;
 
+import com.yangjaehyuk.springboot.config.auth.dto.SessionUser;
 import com.yangjaehyuk.springboot.service.posts.PostsService;
 import com.yangjaehyuk.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +10,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        /*
+             (SessionUser) httpSession.getAttribute("user")
+              - 앞서 작성된 CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성
+              - 로그인 성공 시 httpSession.getAttribute("user")에서 값을 가져올 수 있음
+
+             if (user != null)
+              - 세션에 저장된 값이 있을 때만 model에 userName으로 등록
+              - 세션에 저장된 값이 없으면 model엔 아무런 값이 없는 상태이니 로그인 버튼이 보이게 됨
+        */
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }
