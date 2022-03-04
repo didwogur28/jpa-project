8ca72a5307a0f296261a31c338a80d0c32ed866e
yangjaehyuk <didwogur28@naver.com>
2022-03-04 09:02:04 +0900

    어노테이션 기반 개선-1

---
+++ src/main/java/com/yangjaehyuk/springboot/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,55 @@
+package com.yangjaehyuk.springboot.config.auth;
+
+import com.yangjaehyuk.springboot.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final HttpSession httpSession;
+
+    /**
+     *
+     * @param parameter
+     * @return
+     * 컨트롤러 메서드의 특정 파라미터를 지원하는지 판단
+     * 여기서는 파라미타에 @LoginUser 어노테이션이 붙어 있고, 파라미터 클래스 타입이 SessionUser.class인 경우 true를 반환
+     */
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isuserClass = SessionUser.class.equals(parameter.getParameterType());
+
+        return isLoginUserAnnotation && isuserClass;
+    }
+
+    /**
+     *
+     * @param parameter
+     * @param modelAndViewContainer
+     * @param nativeWebRequest
+     * @param binderFactory
+     * @return
+     * @throws Exception
+     * 파라미터에 전달할 객체를 생성
+     * 여기서는 세션에서 객체를 가져옴
+     */
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer modelAndViewContainer,
+                                  NativeWebRequest nativeWebRequest,
+                                  WebDataBinderFactory binderFactory) throws Exception {
+
+        return httpSession.getAttribute("user");
+    }
+}
